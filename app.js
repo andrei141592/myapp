f81376be2d12afa71a82ba72d5b8424d331000a9
Andrei <andonie.ap@gmail.com>
2020-04-08 16:57:09 +0300

    q

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>My node app</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("App listening on port 5000!");
+});
